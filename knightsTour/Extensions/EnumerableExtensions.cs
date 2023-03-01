692b6c4c77353ead177b60c7fa5cc8181f949b97
duoykb <duoykb@gmail.com>
2023-03-01 11:13:52 +0300

    Added Chain method an extension on IEnumerable instance

---
+++ knightsTour/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace knightsTour.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<TElement> Chain<TElement>(this IEnumerable<TElement> enumerable,
+        IEnumerable<TElement> enumerableTwo)
+    {
+        foreach (var element in enumerable)
+            yield return element;
+
+        foreach (var element in enumerableTwo)
+            yield return element;
+    }
+}
