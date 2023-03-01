753c3beddc4bb7c442093ad098a98961fe1bbce2
duoykb <duoykb@gmail.com>
2023-03-01 09:45:09 +0300

    Initial commit

---
+++ knightsTour/KnightsTourPb.cs
@@ -0,0 +1,33 @@
+﻿namespace knightsTour;
+
+public static class KnightsTourPb
+{
+    public static int MaxLeft(int n) => (MaxRight(n) - 8) + 1;
+    public static int MaxRight(int n) => (int)Math.Ceiling((double)n / 8) * 8;
+
+    public IEnumerable<int>? TwoUpOneOnX()
+    {
+        var maxR = MaxRight(m);
+        var newMr = maxR - 16;
+        if (newMr is > 64 or < 8)
+            return;
+        var twoUp = newMr - (maxR - m);
+        var oneLeft = twoUp - 1;
+        var oneRight = twoUp + 1;
+        
+        if 
+    }
+
+    public static IEnumerable<int>? FindNextMoves(int m)
+    {
+        var twoUpOneLeft = int?() =>
+        {
+
+        }
+    }
+
+    public static IEnumerable<int> FindSequence(int start)
+    {
+        return null;
+    }
+}
---
+++ knightsTour/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+static void DisplayBoard()
+{
+    var moves = Enumerable.Range(1, 64);
+    var movesInARow = 8;
+    var builder = new StringBuilder();
+
+    foreach (var move in moves)
+        if (movesInARow is 1)
+        {
+            builder.Append($"{move}\n");
+            movesInARow = 8;
+        }
+        else
+        {
+            builder.Append($"{move}\t");
+            movesInARow -= 1;
+        }
+    WriteLine($"{builder}");
+}
+DisplayBoard();
+
+WriteLine("---");
+
+
+foreach (var n in new[]{1,4,12,32,57,50})
+{
+    WriteLine($"{n}: maxR {KnightsTourPb.MaxRight(n)}");
+    WriteLine($"{n}: maxL {KnightsTourPb.MaxLeft(n)}");
+}
+
+
